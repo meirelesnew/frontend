3b87354d2e9a27b878162c828b4b6c3538612498
meirelesnew <muty.bjj@gmail.com>
2026-04-23 20:28:34 -0300

    🔥 Migração Firebase: js/config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,12 @@
-// config.js
-const firebaseConfig = {
-  apiKey: "AIzaSyAcH10Rplz_axMnLAd1xE-NUDzIowCRieE",
-  authDomain: "tabuada-17b9e.firebaseapp.com",
-  projectId: "tabuada-17b9e",
-  storageBucket: "tabuada-17b9e.firebasestorage.app",
-  messagingSenderId: "373069312850",
-  appId: "1:373069312850:web:5aa3a46eff9955e742808b",
-  measurementId: "G-FV3XMLQ5NQ"
+const CONFIG = {
+  API_URL: "https://backend-nept.onrender.com/api",
+  FIREBASE: {
+    apiKey:            "AIzaSyAcH10Rplz_axMnLAd1xE-NUDzIowCRieE",
+    authDomain:        "tabuada-17b9e.firebaseapp.com",
+    projectId:         "tabuada-17b9e",
+    storageBucket:     "tabuada-17b9e.firebasestorage.app",
+    messagingSenderId: "373069312850",
+    appId:             "1:373069312850:web:5aa3a46eff9955e742808b",
+    measurementId:     "G-FV3XMLQ5NQ"
+  }
 };
-
-// Exportamos para o resto do sistema usar
-window.firebaseConfig = firebaseConfig;
