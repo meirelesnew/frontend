61f955e3098cc36cf26223e43abedbc03f0c060f
meirelesnew <muty.bjj@gmail.com>
2026-04-23 09:27:02 -0300

    Replace CONFIG with firebaseConfig for Firebase setup
    Removed old CONFIG object and added firebaseConfig for Firebase integration.

--- js/config.js
+++ js/config.js
@@ -1,4 +1,13 @@
-const CONFIG = {
-  API_URL: "https://backend-nept.onrender.com/api"
+// config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAcH10Rplz_axMnLAd1xE-NUDzIowCRieE",
+  authDomain: "tabuada-17b9e.firebaseapp.com",
+  projectId: "tabuada-17b9e",
+  storageBucket: "tabuada-17b9e.firebasestorage.app",
+  messagingSenderId: "373069312850",
+  appId: "1:373069312850:web:5aa3a46eff9955e742808b",
+  measurementId: "G-FV3XMLQ5NQ"
 };
-window.CONFIG = CONFIG;
+
+// Exportamos para o resto do sistema usar
+window.firebaseConfig = firebaseConfig;
